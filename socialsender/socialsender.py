b24239d65efbfe15b580408fda41be121ee26e5d
banksps <phillip@banksnetworking.com>
2013-08-15 21:47:35 -0700

    Added email send code.

--- socialsender/socialsender.py
+++ socialsender/socialsender.py
@@ -3,6 +3,39 @@
 
 import smtplib, sys, os
 
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+sender = 'banksps@gmail.com'
+recipient = 'phillip@banksnetworking.com'
+subj = sys.argv[1]
+body = ""
+user = sys.argv[2]
+pass = sys.argv[3]
+
+
+def email(eaddr,subj,attch,body,user,pass):
+
+    "Sends an e-mail to the specified recipient."
+ 
+    body = "" + body + ""
+ 
+    headers = ["From: " + sender,
+           "Subject: " + subject,
+           "To: " + recipient,
+           "MIME-Version: 1.0",
+           "Content-Type: text/html"]
+    headers = "\r\n".join(headers)
+ 
+    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+ 
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+    session.login(user,pass)
+    session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
+    session.quit()
+
+    
 #Acquire the file
 
 
