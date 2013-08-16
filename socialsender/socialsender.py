fb212d84a9696ce4ea196b4d2b467394785a0b4f
banksps <phillip@banksnetworking.com>
2013-08-15 20:58:14 -0700

    Python email sending.

---
+++ socialsender/socialsender.py
@@ -0,0 +1,21 @@
+#Social networking file analyzer and sender
+#written by Phillip Banks Sr.
+
+import smtplib, sys, os
+
+#Acquire the file
+
+
+#Destinguish what type of file it is
+
+#Are there any associated files
+
+#Determine what social networks this file applies to
+
+#Convert to necessary formats that apply and report status
+    #If there is an error report and move to manual fix folder
+
+#Send to relevant networks from folders and move content for archiving
+	#If there is an error report, send log and stop processing
+	
+#Save log and move to logs folder
